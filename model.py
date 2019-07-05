8c712129144049a56131c22b67fe91b8c35f50a0
ArnaudFickinger <arnaud.fickinger@polytechnique.edu>
2019-07-05 14:11:33 -0400

    good init of weights

--- model.py
+++ model.py
@@ -25,10 +25,14 @@ class VAEOriginal_paper(nn.Module):
         self.encoder = EncoderOriginal(latent_dim, sequence_length, alphabet_size, enc_h1_dim, enc_h2_dim)
 
         self.decoder = SparseDecoderOriginal_paper(latent_dim, sequence_length, alphabet_size, dec_h1_dim, dec_h2_dim, mu_sparse, sigma_sparse, cw_inner_dimension, nb_patterns, hasTemperature, hasDictionary)
+
+
     def forward(self, x):
         # print("---Encoder---")
         mu, logsigma = self.encoder(x)
         z = sample_diag_gaussian_original(mu, logsigma)
+
+
         # z = torch.Tensor([[1.226298, - 0.50005774, - 0.05264941 , 0.66986938 ,- 0.71224884 ,- 1.15921493,
         #   0.05177535 , 1.1583736 ,  0.92446647 ,- 0.14730169  ,0.78172059 , 2.35628846,
         #   1.32076451  ,1.1621947, - 0.12593087 ,- 0.49765921,  0.55820479 ,- 0.84410041,
@@ -40,41 +44,74 @@ class VAEOriginal_paper(nn.Module):
         px_z, logpx_z, mu_W1, logsigma_W1, mu_b1, logsigma_b1, mu_W2, logsigma_W2, mu_b2, logsigma_b2, mu_W3, logsigma_W3, mu_b3, logsigma_b3, mu_S, logsigma_S, mu_C, logsigma_C, mu_l, logsigma_l = self.decoder(z, x)
         return mu, logsigma, px_z, logpx_z, mu_W1, logsigma_W1, mu_b1, logsigma_b1, mu_W2, logsigma_W2, mu_b2, logsigma_b2, mu_W3, logsigma_W3, mu_b3, logsigma_b3, mu_S, logsigma_S, mu_C, logsigma_C, mu_l, logsigma_l
 
+# # Parameter initialization
+#         self.sigma_init = 0.01
+#         self.logsig_init = -5
+#         # Glorot initialization
+#         create_weight = lambda dim_input, dim_output: self.prng.normal(0.0, \
+#             np.sqrt(2.0 / (dim_input + dim_output)), \
+#             (dim_input, dim_output)).astype(theano.config.floatX)
+#         create_weight_zeros = lambda dim_input, dim_output: \
+#             np.zeros((dim_input, dim_output)).astype(theano.config.floatX)
+#         create_bias = lambda dim_output:  0.1 \
+#             * np.ones(dim_output).astype(theano.config.floatX)
+#
+#         # Variational uncertainty
+#         create_weight_logsig = lambda dim_input, dim_output: self.logsig_init \
+#             * np.ones((dim_input, dim_output)).astype(theano.config.floatX)
+#         create_bias_logsig = lambda dim_output: self.logsig_init \
+#             * np.ones(dim_output).astype(theano.config.floatX)
+
 
 class SparseDecoderOriginal_paper(nn.Module):  # sparsity ideas of deep generative model for mutation paper
     def __init__(self, latent_dim, sequence_length, alphabet_size, h1_dim, h2_dim, mu_sparse, sigma_sparse,
                  cw_inner_dimension, nb_patterns, hasTemperature, hasDictionary):
         super(SparseDecoderOriginal_paper, self).__init__()
+
         self.alphabet_size = alphabet_size
         self.sequence_length = sequence_length
         self.nb_patterns = nb_patterns
         self.mu_sparse = mu_sparse
         self.h2_dim = h2_dim
         self.logsigma_sparse = math.log(sigma_sparse)
-        self.mu_W1 = nn.Parameter(torch.Tensor(latent_dim, h1_dim).normal_(0, 0.01))
-        self.logsigma_W1 = nn.Parameter(torch.Tensor(latent_dim, h1_dim).normal_(0, 0.01))
+        self.mu_W1 = nn.Parameter(torch.Tensor(latent_dim, h1_dim))
+        nn.init.xavier_normal_(self.mu_W1)
+        self.logsigma_W1 = nn.Parameter(torch.Tensor(latent_dim, h1_dim))
+        nn.init.constant_(self.logsigma_W1, -5)
         self.mu_b1 = nn.Parameter(
-            torch.Tensor(h1_dim).normal_(0, 0.01))  # try torch.randn(x,x,x)*0.01 if not working
-        self.logsigma_b1 = nn.Parameter(torch.Tensor(h1_dim).normal_(0, 0.01))
-        self.mu_W2 = nn.Parameter(torch.Tensor(h1_dim, h2_dim).normal_(0, 0.01))
-        self.logsigma_W2 = nn.Parameter(torch.Tensor(h1_dim, h2_dim).normal_(0, 0.01))
+            torch.Tensor(h1_dim))
+        nn.init.constant_(self.mu_b1, 0.1)
+        self.logsigma_b1 = nn.Parameter(torch.Tensor(h1_dim))
+        nn.init.constant_(self.logsigma_b1, -5)
+        self.mu_W2 = nn.Parameter(torch.Tensor(h1_dim, h2_dim))
+        nn.init.xavier_normal_(self.mu_W2)
+        self.logsigma_W2 = nn.Parameter(torch.Tensor(h1_dim, h2_dim))
+        nn.init.constant_(self.logsigma_W2, -5)
         self.mu_b2 = nn.Parameter(
-            torch.Tensor(h2_dim).normal_(0, 0.01))  # try torch.randn(x,x,x)*0.01 if not working
-        self.logsigma_b2 = nn.Parameter(torch.Tensor(h2_dim).normal_(0, 0.01))
-        self.mu_W3 = nn.Parameter(torch.Tensor(h2_dim, sequence_length * cw_inner_dimension).normal_(0,
-                                                                                                     0.01))  # try torch.randn(x,x,x)*0.01 if not working
-        self.logsigma_W3 = nn.Parameter(torch.Tensor(h2_dim, sequence_length * cw_inner_dimension).normal_(0, 0.01))
-        self.mu_b3 = nn.Parameter(torch.Tensor(alphabet_size * sequence_length).normal_(0,
-                                                                                        0.01))  # try torch.randn(x,x,x)*0.01 if not working
-        self.logsigma_b3 = nn.Parameter(torch.Tensor(alphabet_size * sequence_length).normal_(0, 0.01))
-        self.mu_S = nn.Parameter(torch.Tensor(int(h2_dim / nb_patterns), sequence_length).normal_(0,
-                                                                                                  0.01))  # try torch.randn(x,x,x)*0.01 if not working
-        self.logsigma_S = nn.Parameter(torch.Tensor(int(h2_dim / nb_patterns), sequence_length).normal_(0, 0.01))
-        self.mu_C = nn.Parameter(torch.Tensor(cw_inner_dimension, alphabet_size).normal_(0,
-                                                                                         0.01))  # try torch.randn(x,x,x)*0.01 if not working
-        self.logsigma_C = nn.Parameter(torch.Tensor(cw_inner_dimension, alphabet_size).normal_(0, 0.01))
-        self.mu_l = nn.Parameter(torch.Tensor(1).normal_(0, 0.01))
-        self.logsigma_l = nn.Parameter(torch.Tensor(1).normal_(0, 0.01))
+            torch.Tensor(h2_dim))
+        nn.init.constant_(self.mu_b2, 0.1)
+        self.logsigma_b2 = nn.Parameter(torch.Tensor(h2_dim))
+        nn.init.constant_(self.logsigma_b2, -5)
+        self.mu_W3 = nn.Parameter(torch.Tensor(h2_dim, sequence_length * cw_inner_dimension))
+        nn.init.xavier_normal_(self.mu_W3)
+        self.logsigma_W3 = nn.Parameter(torch.Tensor(h2_dim, sequence_length * cw_inner_dimension))
+        nn.init.constant_(self.logsigma_W3, -5)
+        self.mu_b3 = nn.Parameter(torch.Tensor(alphabet_size * sequence_length))
+        nn.init.constant_(self.mu_b3, 0.1)
+        self.logsigma_b3 = nn.Parameter(torch.Tensor(alphabet_size * sequence_length))
+        nn.init.constant_(self.logsigma_b3, -5)
+        self.mu_S = nn.Parameter(torch.Tensor(int(h2_dim / nb_patterns), sequence_length))
+        nn.init.zeros_(self.mu_S)
+        self.logsigma_S = nn.Parameter(torch.Tensor(int(h2_dim / nb_patterns), sequence_length))
+        nn.init.constant_(self.logsigma_S, -5)
+        self.mu_C = nn.Parameter(torch.Tensor(cw_inner_dimension, alphabet_size))
+        nn.init.xavier_normal_(self.mu_C)
+        self.logsigma_C = nn.Parameter(torch.Tensor(cw_inner_dimension, alphabet_size))
+        nn.init.constant_(self.logsigma_C, -5)
+        self.mu_l = nn.Parameter(torch.Tensor(1))
+        nn.init.ones_(self.mu_l)
+        self.logsigma_l = nn.Parameter(torch.Tensor(1))
+        nn.init.constant_(self.logsigma_l, -5)
         self.hasTemperature = hasTemperature
         self.hasDictionary = hasDictionary
         if opt.dropout>0:
@@ -187,9 +224,17 @@ class EncoderOriginal(nn.Module):
         self.sequence_length = sequence_length
         self.alphabet_size = alphabet_size
         self.fc1 = nn.Linear(sequence_length*alphabet_size, h1_dim)
+        nn.init.xavier_normal_(self.fc1.weight)
+        nn.init.constant_(self.fc1.bias, 0.1)
         self.fc2 = nn.Linear(h1_dim, h2_dim)
+        nn.init.xavier_normal_(self.fc2.weight)
+        nn.init.constant_(self.fc2.bias, 0.1)
         self.fc3_mu = nn.Linear(h2_dim, latent_dim)
+        nn.init.xavier_normal_(self.fc3_mu.weight)
+        nn.init.constant_(self.fc3_mu.bias, 0.1)
         self.fc3_logsigma = nn.Linear(h2_dim, latent_dim)
+        nn.init.xavier_normal_(self.fc3_logsigma.weight)
+        nn.init.constant_(self.fc3_logsigma.bias, -5)
         if opt.dropout>0:
             self.dropout = nn.Dropout(opt.dropout)
 
